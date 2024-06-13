da239102eade1195c4f6d7f744e73b948c52f097
tridarma6 <triidarma06@gmail.com>
2024-06-14 01:56:59 +0800

    Create : SubscriptionItems Model

---
+++ src/main/java/com/subscription/model/SubscriptionItems.java
@@ -0,0 +1,54 @@
+public class SubscriptionItems{
+    
+    private int subscription;
+    private int item;
+    private int quantity;
+    private int price;
+    private int amount;
+    
+    public SubscriptionItems(){
+        
+    }
+
+    public SubscriptionItems(int subscription, int item, int quantity, int price, int amount) {
+        this.subscription = subscription;
+        this.item = item;
+        this.quantity = quantity;
+        this.price = price;
+        this.amount = amount;
+    }
+    public int getSubscription() {
+        return subscription;
+    }
+    public void setSubscription(int subscription) {
+        this.subscription = subscription;
+    }
+    public int getItem() {
+        return item;
+    }
+    public void setItem(int item) {
+        this.item = item;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public int getAmount() {
+        return amount;
+    }
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+
+
+
+}
