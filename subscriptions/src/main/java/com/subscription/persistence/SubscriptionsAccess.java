954a5128de402de02707d951e5f394cf63635219
PutuRifki <puturifki0@gmail.com>
2024-06-17 03:48:01 +0800

    Modified

--- subscriptions/src/main/java/com/subscription/persistence/SubscriptionsAccess.java
+++ subscriptions/src/main/java/com/subscription/persistence/SubscriptionsAccess.java
@@ -13,6 +13,128 @@ public class SubscriptionsAccess {
         ResultSet result = null;
         ArrayList<Subscriptions> listSubs = new ArrayList<>();
         try {
+<<<<<<< HEAD
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:subscription.db");
+            System.out.println("Connected to database");
+            statement = connection.prepareStatement("SELECT * FROM subscriptions WHERE id = ?");
+            statement.setInt(1, id);
+            result = statement.executeQuery();
+s
+            while (result.next()) {
+                subscription.setId(result.getInt("id"));
+                subscription.setCustomer(result.getInt("customer"));
+                subscription.setBillingPeriod(result.getInt("billing_period"));
+                subscription.setPeriodUnit(result.getString("billing_period_unit"));
+                subscription.setTotalDue(result.getInt("total_due"));
+                subscription.setActiveAt(result.getTimestamp("active_at").toLocalDateTime());
+                subscription.setCurrentTermStart(result.getTimestamp("current_term_start").toLocalDateTime());
+                subscription.setCurrentTermEnd(result.getTimestamp("current_term_end").toLocalDateTime());
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            throw new RuntimeErrorException(new Error(e));
+        } finally {
+            if (result != null) result.close();
+            if (statement != null) statement.close();
+            if (connection != null) connection.close();
+        }
+        return subscription;
+    }
+
+    // Fungsi untuk mendapatkan semua subscriptions
+    public ArrayList<Subscriptions> getAllSubscriptions() throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+        ArrayList<Subscriptions> listSubscriptions = new ArrayList<>();
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:subscription.db");
+            System.out.println("Connected to database");
+            statement = connection.prepareStatement("SELECT * FROM subscriptions");
+            result = statement.executeQuery();
+
+            while (result.next()) {
+                Subscriptions subscription = new Subscriptions();
+                subscription.setId(result.getInt("id"));
+                subscription.setCustomer(result.getInt("customer"));
+                subscription.setBillingPeriod(result.getInt("billing_period"));
+                subscription.setPeriodUnit(result.getString("billing_period_unit"));
+                subscription.setTotalDue(result.getInt("total_due"));
+                subscription.setActiveAt(result.getTimestamp("active_at").toLocalDateTime());
+                subscription.setCurrentTermStart(result.getTimestamp("current_term_start").toLocalDateTime());
+                subscription.setCurrentTermEnd(result.getTimestamp("current_term_end").toLocalDateTime());
+                listSubscriptions.add(subscription);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            throw new RuntimeErrorException(new Error(e));
+        } finally {
+            if (result != null) result.close();
+            if (statement != null) statement.close();
+            if (connection != null) connection.close();
+        }
+        return listSubscriptions;
+    }
+
+    // Fungsi untuk mendapatkan semua subscriptions yang diurutkan berdasarkan current_term_end secara descending
+    public ArrayList<Subscriptions> getAllSubscriptionsSortedByTermEnd() throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+        ArrayList<Subscriptions> listSubscriptions = new ArrayList<>();
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:subscription.db");
+            System.out.println("Connected to database");
+            statement = connection.prepareStatement("SELECT * FROM subscriptions ORDER BY current_term_end DESC");
+            result = statement.executeQuery();
+
+            while (result.next()) {
+                Subscriptions subscription = new Subscriptions();
+                subscription.setId(result.getInt("id"));
+                subscription.setCustomer(result.getInt("customer"));
+                subscription.setBillingPeriod(result.getInt("billing_period"));
+                subscription.setPeriodUnit(result.getString("billing_period_unit"));
+                subscription.setTotalDue(result.getInt("total_due"));
+                subscription.setActiveAt(result.getTimestamp("active_at").toLocalDateTime());
+                subscription.setCurrentTermStart(result.getTimestamp("current_term_start").toLocalDateTime());
+                subscription.setCurrentTermEnd(result.getTimestamp("current_term_end").toLocalDateTime());
+                listSubscriptions.add(subscription);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            throw new RuntimeErrorException(new Error(e));
+        } finally {
+            if (result != null) result.close();
+            if (statement != null) statement.close();
+            if (connection != null) connection.close();
+        }
+        return listSubscriptions;
+    }
+
+    public SubscriptionDetail getSubscriptionDetailById(int id) throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+        SubscriptionDetail subscriptionDetail = new SubscriptionDetail();
+        Subscriptions subscription = new Subscriptions();
+        Customer customer = new Customer();
+        List<SubscriptionItem> subscriptionItems = new ArrayList<>();
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:subscription.db");
+            System.out.println("Connected to database");
+
+            // Get subscription info
+            statement = connection.prepareStatement("SELECT * FROM subscriptions WHERE id = ?");
+            statement.setInt(1, id);
+            result = statement.executeQuery();
+=======
             connection = DriverManager.getConnection("jbdc:sqlite:subscription.db");
             String query = "SELECT s.id as subscription_id, s.customer_id, " +
                            "si.quantity, si.amount, si.item_id, " +
@@ -26,6 +148,7 @@ public class SubscriptionsAccess {
             stmt = connection.prepareStatement(query);
             stmt.setInt(1, id);
             result = stmt.executeQuery();
+>>>>>>> 3dae76aef2b49f8016e7cb531786ab1c090aa50c
 
             if (result.next()) {
                 Customers customer = new Customers();
