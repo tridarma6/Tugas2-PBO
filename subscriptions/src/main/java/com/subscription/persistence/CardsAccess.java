386f0493f354ba4158cc617a6e9c3f93783f1af1
PutuRifki <puturifki0@gmail.com>
2024-06-17 17:45:25 +0800

    Modifikasi Card access : add function delete card if primary

--- subscriptions/src/main/java/com/subscription/persistence/CardsAccess.java
+++ subscriptions/src/main/java/com/subscription/persistence/CardsAccess.java
@@ -44,4 +44,59 @@ public class CardsAccess{
 
         return listCards;
     }
+
+    //  menghapus informasi kartu kredit pelanggan jika is_primary bernilai false
+    public String deleteCard(int customerId, int cardId) throws SQLException {
+        Connection conn = null;
+        PreparedStatement selectState = null;
+        PreparedStatement deleteState = null;
+        String response;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:subscription.db");
+            System.out.println("Connected to the database");
+
+            // Cek jika kartu adalah primary
+            String selectQuery = "SELECT is_primary FROM cards WHERE id = ? AND customer_id = ?";
+            selectState = conn.prepareStatement(selectQuery);
+            selectState.setInt(1, cardId);
+            selectState.setInt(2, customerId);
+            ResultSet rs = selectState.executeQuery();
+
+            if (rs.next()) {
+                int isPrimary = rs.getInt("is_primary");
+                if (isPrimary == 0) {
+                    // Hapus Card jika tidak primary
+                    String deleteQuery = "DELETE FROM cards WHERE id = ? AND customer_id = ?";
+                    deleteState = conn.prepareStatement(deleteQuery);
+                    deleteState.setInt(1, cardId);
+                    deleteState.setInt(2, customerId);
+                    int rowsAffected = deleteState.executeUpdate();
+
+                    if (rowsAffected > 0) {
+                        response = rowsAffected + " row(s) have been affected";
+                        System.out.println(response);
+                    } else {
+                        response = "No rows have been affected";
+                        System.out.println(response);
+                    }
+                } else {
+                    response = "Cannot delete primary card";
+                    System.out.println(response);
+                }
+            } else {
+                response = "Card not found";
+                System.out.println(response);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            throw new RuntimeException(e);
+        } finally {
+            if (selectState != null) selectState.close();
+            if (deleteState != null) deleteState.close();
+            if (conn != null) conn.close();
+        }
+        return response;
+    }
 }
